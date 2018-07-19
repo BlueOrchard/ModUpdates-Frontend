555cfb9993e4eb4215b0b87994a6d9524a17978f
Joseph Richey <josephrichey@me.com>
2018-07-19 17:56:30 -0400

    Added header and logo

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="header">
+        <div class="logo">
+            <img src="./../assets/logo.png">
+        </div>
+
+        <div class="search">
+            <input type="text" placeholder="Search ModUpdates">
+            <button><i class="fas fa-search"></i></button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
