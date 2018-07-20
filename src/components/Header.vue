398586591ed55fb2070acd1f38576a54f87bc0ee
Joseph Richey <josephrichey@me.com>
2018-07-20 10:55:12 -0400

    Added browse link, subscriptions link, and profile link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,22 @@
             <input type="text" placeholder="Search ModUpdates">
             <button><i class="fas fa-search"></i></button>
         </div>
+
+        <div class="nav-menu">
+            <a href="">Browse</a>
+            <a href="">Subscriptions</a>
+        </div>
+
+        <div class="right-profile">
+            <div class="avatar-name">
+                <p>Login</p>
+            </div>
+            <div class="avatar">
+                <div class="white-border">
+                    <img src="./../assets/avatar.jpg">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
