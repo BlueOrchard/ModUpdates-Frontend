783860fda530ea6804d4f294598c03c69d2520dd
Joseph <josephrichey@me.com>
2018-07-23 20:48:55 -0400

    Hooked new templates into main app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
     <Header />
+    <div class="body-content">
+      <SideBar/>
+      <Body/>
+    </div>
+
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Body from './components/Body.vue';
+import SideBar from './components/SideBar.vue';
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header, Footer, Body, SideBar
   }
 }
 </script>
