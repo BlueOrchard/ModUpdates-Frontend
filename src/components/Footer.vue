34c510efcc6da7ef28e1ac735f9234870025923e
Joseph Richey <josephrichey@me.com>
2018-07-24 15:52:00 -0400

    Added initial footer changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,30 @@
 <template>
     <div class="footer">
-        Footer
+        <div class="logo-part">
+            <img src="./../assets/logo.png">
+            <p>Copyright© ModUpdates. ModUpdates is not affiliated with Mojang AB or Microsoft.</p>
+            <a href="">Terms of Service</a>
+            <a href="">Privacy Policy</a>
+        </div>
+
+        <div class="nav-part">
+            <a href=""><i class="fas fa-code-branch"></i> Version 1.13</a>
+            <a href=""><i class="fas fa-code-branch"></i> Version 1.11</a>
+            <a href=""><i class="fas fa-code-branch"></i> Version 1.9</a>
+            <a href=""><i class="fas fa-code-branch"></i> Version 1.8</a>
+            <a href=""><i class="fas fa-code-branch"></i> Version 1.7</a>
+        </div>
+
+        <div class="nav-part">
+            <a href=""><i class="fas fa-user"></i> Your Profile</a>
+            <a href=""><i class="fas fa-list-ul"></i> Your Subscriptions</a>
+            <a href=""><i class="fas fa-user-cog"></i> Settings</a>
+        </div>
+
+        <div class="nav-part">
+            <a href="https://github.com/BlueOrchard/ModUpdates-Frontend" target="_blank"><i class="fab fa-github"></i> Github</a>
+            <a href="https://trello.com/b/wygqoMHG/modupdates-roadmap" target="_blank"><i class="fab fa-trello"></i> Roadmap</a>
+        </div>
     </div>
 </template>
 
